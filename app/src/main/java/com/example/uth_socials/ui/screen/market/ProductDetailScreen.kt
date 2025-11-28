a5263c752f4e2eb43b5b61e013b13ece26133120
Thienhd68 <thienhuynh.07072020@gmail.com>
2025-11-28 15:29:14 +0700

    Fix lỗi git2

--- app/src/main/java/com/example/uth_socials/ui/screen/market/ProductDetailScreen.kt
+++ app/src/main/java/com/example/uth_socials/ui/screen/market/ProductDetailScreen.kt
@@ -50,6 +50,7 @@ import com.example.uth_socials.R
 import com.example.uth_socials.data.market.Product
 import com.example.uth_socials.data.user.User
 import com.example.uth_socials.ui.component.common.CallConfirmDialog
+import com.example.uth_socials.ui.component.common.Call_Chat
 import com.example.uth_socials.ui.component.common.formatTimeAgo
 import com.example.uth_socials.ui.component.common.formatVND
 import com.example.uth_socials.ui.viewmodel.MarketViewModel
@@ -332,7 +333,7 @@ fun ImageCarousel(imageUrls: List<String>) {
         if (images.size > 1) {
             Surface(
                 modifier = Modifier
-                    .align(Alignment.TopEnd)
+                    .align(Alignment.BottomEnd)
                     .padding(16.dp),
                 shape = RoundedCornerShape(12.dp),
                 color = Color.Black.copy(alpha = 0.6f)
@@ -496,55 +497,6 @@ fun InfoChip(
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
-        }
-    }
-}
-
-//Btn Call & Chat
-@Composable
-fun Call_Chat(
-    onCall: () -> Unit = {},
-    onMessage: () -> Unit = {}
-) {
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp),
-        contentAlignment = Alignment.Center
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp),
-            horizontalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            Button(
-                onClick = onCall,
-                modifier = Modifier
-                    .weight(1f)
-                    .height(50.dp),
-                shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFE6E0E9))
-            ) {
-                Text("Gọi",
-                    color = Color.Black,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Medium)
-            }
-            Button(
-                onClick = onMessage,
-                modifier = Modifier
-                    .weight(1f)
-                    .height(50.dp),
-                shape = RoundedCornerShape(12.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF0E9397))
-            ) {
-                Text("Nhắn tin",
-                    color = Color.Black,
-                    fontSize = 18.sp,
-                    fontWeight = FontWeight.Medium
-                )
-            }
         }
     }
 }
